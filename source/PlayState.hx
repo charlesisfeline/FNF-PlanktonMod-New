cff367a062b3a1004ec711d91066399f3cc46309
CharlesCat <62410083+SonicProYT@users.noreply.github.com>
2022-01-29 18:33:37 -0500

    more accurate dave and bambi icon

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4597,9 +4597,10 @@ class PlayState extends MusicBeatState
 
 		var funny:Float = (healthBar.percent * 0.01) + 0.01;
 
-		iconP1.setGraphicSize(Std.int(iconP1.width + (50 * funny)),Std.int(iconP2.height - (25 * funny)));
+		//icon squish funny haha
+		iconP1.setGraphicSize(Std.int(iconP1.width + (50 * (2 - funny))),Std.int(iconP1.height - (25 * (2 - funny))));
 		iconP2.setGraphicSize(Std.int(iconP2.width + (50 * (2 - funny))),Std.int(iconP2.height - (25 * (2 - funny))));
-	
+
 		iconP1.updateHitbox();
 		iconP2.updateHitbox();
 
